2e2235de6e3829ef100f6fd4e6ceb422e2a73542
koki-develop <kou.pg.0131@gmail.com>
2023-03-14 20:24:52 +0900

    set description

--- cmd/root.go
+++ cmd/root.go
@@ -8,13 +8,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "typingo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Typing game written in Go",
 }
 
 func Execute() {
