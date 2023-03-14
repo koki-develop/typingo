99370c36fd55f7d791058005d60ffa0e31701287
koki-develop <kou.pg.0131@gmail.com>
2023-03-14 21:01:01 +0900

    increment index when enter pressed

--- internal/game/update.go
+++ internal/game/update.go
@@ -8,6 +8,9 @@ func (g *Game) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.Type {
 		case tea.KeyCtrlC:
 			return g, tea.Quit
+		case tea.KeyEnter:
+			g.currentIndex++
+			return g, nil
 		}
 	}
 
