0cca8cf368dbf526823a66eb7222cdbdf7dec133
koki-develop <kou.pg.0131@gmail.com>
2023-03-15 21:59:35 +0900

    add --num-words flag

--- cmd/root.go
+++ cmd/root.go
@@ -7,12 +7,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	numWords int
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "typingo",
 	Short: "Typing game written in Go",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		m := game.New(&game.GameConfig{
-			NumWords: 10,
+			NumWords: numWords,
 		})
 		if err := game.Run(m); err != nil {
 			return err
@@ -26,4 +30,8 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.Flags().IntVar(&numWords, "num-words", 10, "the number of words")
 }
