4c5ee9581d01706b14e82b9d7a990f1cd50f3d44
koki-develop <kou.pg.0131@gmail.com>
2023-03-16 21:21:15 +0900

    set version

--- cmd/root.go
+++ cmd/root.go
@@ -3,12 +3,15 @@ package cmd
 import (
 	"errors"
 	"os"
+	"runtime/debug"
 
 	"github.com/koki-develop/typingo/internal/game"
 	"github.com/spf13/cobra"
 )
 
 var (
+	version string
+
 	numTexts int
 	beep     bool
 )
@@ -40,6 +43,14 @@ func Execute() {
 }
 
 func init() {
+	if version == "" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			version = info.Main.Version
+		}
+	}
+
+	rootCmd.Version = version
+
 	rootCmd.Flags().IntVarP(&numTexts, "num-texts", "n", 10, "the number of texts")
 	rootCmd.Flags().BoolVarP(&beep, "beep", "b", true, "whether to beep when mistaken")
 }
